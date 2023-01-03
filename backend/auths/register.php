962ee9732283a0b399f424d7c28eeae0dedd1721
Luis-Rojas-H <larojash@uni.pe>
2023-01-03 12:46:51 -0500

    no registrar repetidos primer avance

--- backend/auths/register.php
+++ backend/auths/register.php
@@ -3,6 +3,16 @@
 $email = strtoupper($_POST["email_uni"]);
 $clave = $_POST["pass_uni"];
 
+function obtener_usuario($e_uni){
+    include_once ("../../conf/conexion.php");
+    $conexion=conectar();
+
+    $sql = "SELECT * FROM usuario WHERE email_uni = '".$e_uni."';";
+    $resp = mysqli_query($conexion, $sql);
+    $num = mysqli_num_rows($resp);
+    return $num;
+}
+
 function error(){
     header("Location:http://localhost/EDUNI/EDUNI/index.html?error=1");
 }
@@ -11,13 +21,17 @@ function Registro($email,$clave){
     include_once ("../../conf/conexion.php");
     $conexion=conectar();
 
-    $sqlr="insert into usuario (id, email_uni, pass_uni) values (
+    if(obtener_usuario($email)){
+        $sqlr="insert into usuario (id, email_uni, pass_uni) values (
 		    NULL,
 			'".$email."',
 			'".$clave."'
         );";
+        $resp = mysqli_query($conexion,$sqlr);
+    } else {
+        header("Location:http://localhost/EDUNI/EDUNI/index.html?error=2");
+    }
 
-    $resp = mysqli_query($conexion,$sqlr);
     if (!isset($resp)) {
         header("Location:http://localhost/EDUNI/EDUNI/index.html?error=1");
     } else {
